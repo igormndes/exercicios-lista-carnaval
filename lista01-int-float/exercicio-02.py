1a90fc077be26c6682d1e4b23cfd5af432e0bc25
Igor Mendes <92588244+igormndes@users.noreply.github.com>
2022-03-04 20:35:52 -0300

    Create exercicio-02.py

---
+++ lista01-int-float/exercicio-02.py
@@ -0,0 +1,23 @@
+"""
+How Bootcamps - Stone - /código[s]
+Data: 02/03/2022
+Autor: Igor Gabryell Mendes Melo
+Descrição: Exercicio proposto pelo instrutor Henrique Branco.
+Lista de exercícios
+Tema: Tipos numéricos
+Diversão do carnaval \o/
+
+Exercício 2: 
+Faça um programa que receba a base e altura de um triângulo e imprima a área dele.
+
+"""
+# Solicitando base e altura do triângulo:
+b = float(input("Digite o comprimento da base do triângulo: "))
+
+h = float(input("Digite o comprimento da altura do triângulo: "))
+
+
+# Calculando a área do triângulo:
+
+area = (b * h) / 2
+print (f"A área do triângulo é: {area} m²\n")
