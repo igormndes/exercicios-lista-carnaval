02fc91d2df25705fa3cd2a384e271366ab8dda49
Igor Mendes <92588244+igormndes@users.noreply.github.com>
2022-03-04 20:39:09 -0300

    Create exercicio-02.py

---
+++ lista02- litas/exercicio-02.py
@@ -0,0 +1,24 @@
+"""
+How Bootcamps - Stone - /código[s]
+Data: 04/03/2022
+Autor: Igor Gabryell Mendes Melo
+Descrição: Exercicio proposto pelo instrutor Henrique Branco.
+Lista de exercícios
+Tema: Listas
+Diversão do carnaval \o/
+
+Exercício 2:
+Crie uma lista com 10 elementos (você escolhe quais serão) e imprima a lista na ordem inversa. Exemplo: para a lista [1, 3, 6, “H”, [7,7,7] a saída deve ser
+>>> [[7,7,7], “H”, 6, 3, 1]
+
+"""
+
+# Definindo lista
+lista = ["Igor", "Mendes", "Melo", 25, "Casado", 6, (7,7,7), [8,8,8], "nove", 10]
+
+
+# Invertendo lista
+lista.reverse()
+
+# Imprindo lista na ordem inversa
+print (lista)
